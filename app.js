ec9da8a213d23bee6367777c5d9641515a5d069b
Mehul Tandel <mehultandel.tech@gmail.com>
2022-04-21 13:45:06 +0530

    added static files to serve

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,13 +10,14 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res)=>{
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
+// __dirname gives path to folder where THIS file exists
+// .. goes up one level in folder hierarchy
 router.get('/',(req,res)=>{
-    res.send('<h1>Hello</h1>');
+    res.sendFile(path.join(__dirname,'..','views','shop.html')); 
 });
 
 module.exports = router;
