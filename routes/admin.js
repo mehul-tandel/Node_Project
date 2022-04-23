15bef349a9cdd0973047956422c94725fa5c9e32
Mehul Tandel <mehultandel.tech@gmail.com>
2022-04-23 12:49:38 +0530

    Seperated controllers

--- routes/admin.js
+++ routes/admin.js
@@ -2,28 +2,17 @@ const path = require('path');
 
 const express = require('express');
 
+const productsController = require('../controllers/products');
+
 const router = express.Router();
 
-const products = [];
+
 
 // /admin/add-product --> GET
 // render(template,{object with contains variables to be passed into the template})
-router.get('/add-product',(req,res)=>{
-    res.render('add-product',{
-        pageTitle: 'Add Product',
-        path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
-    });
-});
+router.get('/add-product', productsController.getAddProduct); //only reference to the function has to be provided(not executed)
 
 // /admin/add-product  --> POST
-router.post('/add-product', (req,res)=>{
-    products.push({title: req.body.title});
-    res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-// module.exports.routes = router
-exports.routes = router;
-exports.products = products;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,15 +2,11 @@ const path = require('path');
 
 const express = require('express');
 
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-// __dirname gives path to folder where THIS file exists
-// .. goes up one level in folder hierarchy
-router.get('/',(req,res)=>{
-    const products = adminData.products;
-    res.render('shop', {prods: products, pageTitle: 'Shop',path: '/'});
-});
+
+router.get('/',productsController.getProducts);
 
 module.exports = router;
