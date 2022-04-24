2fca18954633b652d1d951ca2dc8f5bf71ce8c41
Mehul Tandel <mehultandel.tech@gmail.com>
2022-04-24 19:14:58 +0530

    add to cart functionality added

--- models/product.js
+++ models/product.js
@@ -40,7 +40,7 @@ module.exports = class Product {
 
     static findById(id,cb){
         getProductsFromFile(products =>{
-            const product = products.find(p => p.id===id);
+            const product = products.find(p =>p.id===id);
             cb(product); 
         });
     }
