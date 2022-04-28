aa615d51be35098636f441be3c83690a1cacbb25
Mehul Tandel <mehultandel.tech@gmail.com>
2022-04-28 16:20:03 +0530

    using mongoose created product model

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const keys = require('./keys');
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -20,21 +21,24 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use((req,res,next)=>{
-    User.findByPk('626935995be3f0e0362becd9')
-    .then(user =>{
-        req.user = new User(user.name, user.email, user.cart, user._id);
-        next();
-    })
-    .catch(err=>console.log(err));
-});
+// app.use((req,res,next)=>{
+//     User.findByPk('626935995be3f0e0362becd9')
+//     .then(user =>{
+//         req.user = new User(user.name, user.email, user.cart, user._id);
+//         next();
+//     })
+//     .catch(err=>console.log(err));
+// });
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-
+mongoose
+.connect('mongodb+srv://mehul_tandel31:'+keys.cluster0+'@cluster0.mr78j.mongodb.net/shop?retryWrites=true&w=majority')
+.then(result => {
     app.listen(3000);
+}).catch(err => {
+    console.log(err);
 });
